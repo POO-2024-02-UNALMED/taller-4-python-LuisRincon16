c19d4a320a75b3a4780dedba317a884dc052ec84
LuisRincon16 <lurinconj@unal.edu.co>
2024-11-24 19:34:02 +0000

    int3

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,13 +3,13 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
         
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
